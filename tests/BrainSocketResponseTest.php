e19ed99414d99c586aa9931c9a598e172eab5957
Andrew Wood <andygwood@gmail.com>
2013-10-29 19:30:34 -0400

    added BrainSocketResponse unit tests.

---
+++ tests/BrainSocketResponseTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use BrainSocket\BrainSocketResponse;
+
+class BrainSocketResponseTest extends PHPUnit_Framework_TestCase{
+
+	protected function tearDown(){
+		Mockery::close();
+	}
+
+	protected function mockEventPublisher(){
+		$mock = Mockery::mock('BrainSocket\LaravelEventPublisher');
+		return $mock;
+	}
+
+	public function testResponseReturnsObjectWithAClientProperty(){
+		$mock = $this->mockEventPublisher();
+		$mock->shouldReceive('fire')->once();
+		$BrainSocketResponse = new BrainSocketResponse($mock);
+
+		$response = $BrainSocketResponse->make('hello.world');
+
+		$response = json_decode($response);
+
+		return $this->assertObjectHasAttribute('client',$response);
+	}
+
+	public function testResponseExceptsJSONStringAndReturnsServerDataWhenEventIsFired(){
+
+		$client_object = (object)array(
+			'client' => (object)array(
+					'event' => 'hello.world'
+				)
+		);
+
+		$mock = $this->mockEventPublisher();
+		$mock->shouldReceive('fire')->once()->with($client_object->client->event,array($client_object->client))->andReturn('foo');
+		$BrainSocketResponse = new BrainSocketResponse($mock);
+
+		$response = $BrainSocketResponse->make(json_encode($client_object));
+
+		$response = json_decode($response);
+
+		return $this->assertObjectHasAttribute('server',$response);
+	}
+
+}
